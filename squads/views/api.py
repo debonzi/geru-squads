e12fc72d09328ff3c3ae68da6cdc66761a8d9fdc
Daniel Debonzi <debonzi@geru.com.br>
2018-09-03 18:52:34 -0300

    Preparing to Heroku.

--- squads/views/api.py
+++ squads/views/api.py
@@ -1,6 +1,9 @@
+from pyramid import httpexceptions
 from squads.storage.mongodb import mongo
 
 def get_squads(request):
     squads = mongo.db.squads.find_one()
+    if not squads:
+        raise httpexceptions.HTTPNotFound()
     squads.pop('_id')
     return squads
