c1f49423162b8a3f0c19ba1e002ba8278cb17a3b
Daniel Debonzi <debonzi@geru.com.br>
2018-09-03 19:11:15 -0300

    Configure MongoDB.

--- squads/__init__.py
+++ squads/__init__.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
+import os
 from pyramid.config import Configurator
 
 from squads.storage.mongodb import mongo
 from squads.views.api import get_squads
 
 def main(global_config, **settings):
+    envvar_mongo_uri = os.environ.get('MONGODB_URI', None)
+    if envvar_mongo_uri:
+        settings.update(
+            {
+                'mongo.host': envvar_mongo_uri
+            }
+        )
     config = Configurator(settings=settings)
 
     mongo.configure(settings)
